5034da52b374cfd76a598d4279a34d72ad303baf
zs23017357 <28128346-zs23017357@users.noreply.replit.com>
2024-02-16 18:53:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Algoritmo de Euclides
+"""
+a = int(input("Dame primer número: "))
+b = int(input("Dame segundo número: "))
+
+def euclides(a, b):
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    else:
+        r = a % b
+        return euclides(b, r)
+
+print("El MCD de", a, "y", b, "es:", euclides(a, b))
